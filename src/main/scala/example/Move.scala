71c6884bd9a413591016e00339b5544a4e37301b
aSlug <marpi93@icloud.com>
2020-02-14 14:52:33 +0100

    remove unused Move.fromInt

--- src/main/scala/example/Move.scala
+++ src/main/scala/example/Move.scala
@@ -6,15 +6,6 @@ object Move {
     case object Paper extends EnumVal
     case object Scissors extends EnumVal
 
-  def fromInt(i: Int): Option[Move.EnumVal] = {
-    i match {
-      case 0 => Some(Move.Rock)
-      case 1 => Some(Move.Paper)
-      case 2 => Some(Move.Scissors)
-      case _ => None
-    }
-  }
-
   def fromString(i: String): Option[Move.EnumVal] = {
     i match {
       case "0" => Some(Move.Rock)
