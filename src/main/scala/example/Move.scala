39c99ec101dd220b97deedec07858efc6787e6c4
aSlug <marpi93@icloud.com>
2020-02-14 11:33:47 +0100

    cerate Move model

---
+++ src/main/scala/example/Move.scala
@@ -0,0 +1,27 @@
+package example
+
+object Move {
+    sealed trait EnumVal
+    case object Rock extends EnumVal
+    case object Paper extends EnumVal
+    case object Scissors extends EnumVal
+
+  def fromInt(i: Int): Option[Move.EnumVal] = {
+    i match {
+      case 0 => Some(Move.Rock)
+      case 1 => Some(Move.Paper)
+      case 2 => Some(Move.Scissors)
+      case _ => None
+    }
+  }
+
+  def fromString(i: String): Option[Move.EnumVal] = {
+    i match {
+      case "0" => Some(Move.Rock)
+      case "1" => Some(Move.Paper)
+      case "2" => Some(Move.Scissors)
+      case _ => None
+    }
+}
+
+
