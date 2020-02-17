a2aa26a2c6f5d4aa87fc42769d3903d8a5dff3f9
aSlug <marpi93@icloud.com>
2020-02-17 16:36:36 +0100

    add akka-http and make the app work with http requests

--- src/main/scala/rpsls/RPSLS.scala
+++ src/main/scala/rpsls/RPSLS.scala
@@ -1,15 +1,34 @@
 package rpsls
 
-object RPSLS extends Greeting with GameProposal with App {
-  println(greeting)
-  println(gameProposal)
-  Game.play()
-}
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import scala.io.StdIn
 
-trait Greeting {
-  lazy val greeting: String = "hello"
-}
+import de.heikoseeberger.akkahttpcirce.ErrorAccumulatingCirceSupport._
+import io.circe.generic.auto._
+import io.buildo.enumero.circe._
 
-trait GameProposal {
-  lazy val gameProposal: String = "wanna play?"
+object RPSLS extends App {
+  implicit val system = ActorSystem("rps")
+  implicit val materializer = ActorMaterializer()
+  implicit val executionContext = system.dispatcher
+
+  val route =
+    path("play") {
+        get {
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "Ciao ciao"))
+        }
+    }
+
+  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+  StdIn.readLine() // let it run until user presses return
+  bindingFuture
+    .flatMap(_.unbind()) // trigger unbinding from the port
+    .onComplete(_ => system.terminate()) // and shutdown when done
+
 }
