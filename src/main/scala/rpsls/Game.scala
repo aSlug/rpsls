ff4bd4ff7d7d1c0a849cce6c1644998986e16b03
aSlug <marpi93@icloud.com>
2020-02-17 18:44:34 +0100

    add wiro (not working!)

--- src/main/scala/rpsls/Game.scala
+++ src/main/scala/rpsls/Game.scala
@@ -2,24 +2,35 @@ package rpsls
 
 import rpsls.Move._
 import rpsls.Result._
+import rpsls.Response
 
 import scala.util.Random
 
-object Game {
-  def play(playerMove: Move): (Move, Move, Result) = {
+import scala.concurrent.{ExecutionContext, Future}
+import wiro.annotation._
+
+@path("rps")
+trait GameAPI {
+  @command
+  def play( userMove: Move ): Future[Either[Error, Response]]
+}
+
+class GameAPIImpl(implicit exc: ExecutionContext) extends GameAPI {
+  override def play(playerMove: Move): Future[Either[Error, Response]] = Future {
     val botMove = generateBotMove()
-    return outcome(playerMove, botMove)
+    val result = outcome(playerMove, botMove)
+    return Right(Response(userMove = playerMove, computerMove = botMove, result = result))
   }
 
   def generateBotMove(): Move = {
     Random.shuffle(List(Rock, Paper, Scissors)).head
   }
 
-  def outcome(playerMove: Move, botMove: Move): (Move, Move, Result) = {
+  def outcome(playerMove: Move, botMove: Move): Result = {
     (playerMove, botMove) match {
-      case (playerMove, botMove) if playerMove == botMove => (playerMove, botMove, Draw)
-      case (Rock, Scissors) | (Scissors, Paper) | (Paper, Rock) => (playerMove, botMove, Win)
-      case _ => (playerMove, botMove, Lose)
+      case (playerMove, botMove) if playerMove == botMove => Draw
+      case (Rock, Scissors) | (Scissors, Paper) | (Paper, Rock) => Win
+      case _ => Lose
     }
   }
 
