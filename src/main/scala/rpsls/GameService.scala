a3843a154dd1709403ab3f4ae0257d5fdaf00a74
aSlug <marpi93@icloud.com>
2020-02-20 18:32:04 +0100

    implement service

--- src/main/scala/rpsls/GameService.scala
+++ src/main/scala/rpsls/GameService.scala
@@ -13,14 +13,21 @@ trait GameService {
 
 class GameServiceImpl(repo: GameRepo) extends GameService {
 
-  override def makePlay(playerMove: Move): Unit = ???
-  override def getResult(): Option[Game] = ???
+  override def makePlay(playerMove: Move): Unit = {
+    val botMove = generateBotMove();
+    val outcome = calculateOutcome(playerMove, botMove)
+    repo.write(Game(playerMove, botMove, outcome))
+  }
+
+  override def getResult(): Option[Game] = {
+    repo.read()
+  }
 
   private def generateBotMove(): Move = {
     Random.shuffle(List(Rock, Paper, Scissors)).head
   }
 
-  private def outcome(playerMove: Move, botMove: Move): Outcome = {
+  private def calculateOutcome(playerMove: Move, botMove: Move): Outcome = {
     (playerMove, botMove) match {
       case (playerMove, botMove) if playerMove == botMove       => Draw
       case (Rock, Scissors) | (Scissors, Paper) | (Paper, Rock) => Win
